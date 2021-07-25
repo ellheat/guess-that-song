42718da2acf025a64e8b537ccde266a30c7f7956
ellheat <ppoleszczuk@apptension.com>
2021-07-25 16:24:03 +0200

    Create characters

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
-const app = express();
-const PORT = 8080;
+import { colors, Ports } from './config';
+import { getCharacters } from './components/characters';
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+const app = express();
+
+app.listen(Ports.Base, () => {
+  console.log(colors.success(`Backend listening on port ${Ports.Base}!`));
+  getCharacters().then(() => console.log(colors.success('Characters created')));
 });
