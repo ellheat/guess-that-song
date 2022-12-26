6148e6f7ef967a9b7d046b46c9d60f4a9dc39274
Patryk Poleszczuk <patryk.poleszczuk@gmail.com>
2022-12-26 21:56:35 +0100

    Fetch preview url when it does not exists

--- packages/backend/config/base.ts
+++ packages/backend/config/base.ts
@@ -2,5 +2,5 @@ export const config = {
     maxRounds: 30,
     roundTimer: 10,
     preRoundTimer: 3,
-    playlists: ['1Gl2k2c3CEsHM0cEY600jw', '7v4szGxFiDrRKlgVxRy8yy'],
+    playlists: ['1Gl2k2c3CEsHM0cEY600jw'],
 };
--- packages/backend/modules/answers.ts
+++ packages/backend/modules/answers.ts
@@ -1,8 +1,8 @@
 import { config } from '../config';
-import { Spotify } from './spotify';
 import { getRandomNumber } from '../utils/randomRangeNumber';
 import { TrackType } from '../types/track';
 import { shuffle } from '../utils/array';
+import { Spotify } from './spotify';
 
 const ANSWERS_COUNT = 4;
 
@@ -15,7 +15,23 @@ export class Answers {
         this.spotify = spotify;
     }
 
-    getTrack = (roundArray: TrackType[], playlist: TrackType[], index: number) => {
+    // getTrackOld = (roundArray: TrackType[], playlist: TrackType[], index: number) => {
+    //     const trackIndex: number = getRandomNumber(0, playlist.length - 1);
+    //     const track: TrackType = Object.assign({ isCorrect: index === 0 }, <TrackType>playlist[trackIndex]);
+    //     const isSameSong: boolean = roundArray.some((item) => item.id === track.id);
+
+    //     if (index === 0) {
+    //         playlist.splice(trackIndex, 1);
+    //     }
+
+    //     if (isSameSong) {
+    //         this.getTrackOld(roundArray, playlist, index);
+    //     } else {
+    //         roundArray.push(track);
+    //     }
+    // };
+
+    getTrack = async (roundArray: TrackType[], playlist: TrackType[], index: number) => {
         const trackIndex: number = getRandomNumber(0, playlist.length - 1);
         const track: TrackType = Object.assign({ isCorrect: index === 0 }, <TrackType>playlist[trackIndex]);
         const isSameSong: boolean = roundArray.some((item) => item.id === track.id);
@@ -25,60 +41,81 @@ export class Answers {
         }
 
         if (isSameSong) {
-            this.getTrack(roundArray, playlist, index);
-        } else {
-            // if (!track.previewUrl && track.isCorrect) {
-            //     console.log('track', track);
-            //     await this.spotify.api.searchTracks(`${track.artist} ${track.title}`).then((data: any) => {
-            //         const song = data.body.tracks.items.find(
-            //             (deepTrack: any) =>
-            //                 deepTrack.artists[0].name === track.artist &&
-            //                 deepTrack.name.includes(track.title) &&
-            //                 deepTrack.duration_ms === track.duration,
-            //         );
-            //         if (song) {
-            //             console.log('song', song);
+            console.log('isSameSong');
 
-            //             roundArray.push({
-            //                 album: song.album.name,
-            //                 artist: song.artists[0].name,
-            //                 duration: song.duration_ms,
-            //                 id: song.id,
-            //                 isCorrect: true,
-            //                 previewUrl: song.preview_url,
-            //                 title: song.name,
-            //                 url: song.uri,
-            //             });
-            //         } else {
-            //             // console.log('song else');
-            //             this.getTrack(roundArray, playlist, index);
-            //         }
-            //     });
-            // }
-            // console.log('else pushed');
-            roundArray.push(track);
+            await this.getTrack(roundArray, playlist, index);
+        } else {
+            if (!track.previewUrl && track.isCorrect) {
+                console.log('if');
+
+                await this.spotify.api.searchTracks(`${track.artist} ${track.title}`).then(async (data: any) => {
+                    const song = data.body.tracks.items.find(
+                        (deepTrack: any) =>
+                            deepTrack.artists[0].name === track.artist &&
+                            deepTrack.name.includes(track.title) &&
+                            deepTrack.duration_ms === track.duration,
+                    );
+                    if (song) {
+                        console.log('song');
+
+                        roundArray.push({
+                            album: song.album.name,
+                            artist: song.artists[0].name,
+                            duration: song.duration_ms,
+                            id: song.id,
+                            isCorrect: true,
+                            previewUrl: song.preview_url,
+                            title: song.name,
+                            url: song.uri,
+                        });
+                    } else {
+                        console.log('song else');
+
+                        await this.getTrack(roundArray, playlist, index);
+                    }
+                });
+            } else {
+                console.log('else pushed');
+                roundArray.push(track);
+            }
         }
     };
 
-    prepare = () => {
+    prepare = async () => {
         console.log('preparing answers...');
-        this.answers = [];
         const playlists = this.spotify.getPlaylists();
+        const rounds = new Array(config.maxRounds / playlists.length).fill([]);
 
-        playlists.forEach((playlist: TrackType[]) => {
-            for (let index = 0; index < config.maxRounds / playlists.length; index++) {
-                const tracksArray: TrackType[] = [];
-                for (let answerIndex = 0; answerIndex < ANSWERS_COUNT; answerIndex++) {
-                    this.getTrack(tracksArray, playlist, answerIndex);
-                }
-                const round: TrackType[] = shuffle(tracksArray);
-                this.answers.push(round);
-            }
-        });
+        const answersArray = await Promise.all(
+            playlists.map(async (playlist: TrackType[]) => {
+                return await Promise.all(
+                    rounds.map(async () => {
+                        const answers = [...Array(ANSWERS_COUNT).keys()];
+                        const tracksArray: TrackType[] = [];
 
-        const correctAnswers = this.answers.flat().filter((answer) => answer.isCorrect);
-        console.log('this.answers', this.answers.length);
-        console.log('correctAnswers', correctAnswers.length);
+                        for (const answer in answers) {
+                            await this.getTrack(tracksArray, playlist, Number(answer));
+                        }
+
+                        return shuffle(tracksArray);
+                    }),
+                );
+            }),
+        );
+
+        // playlists.forEach((playlist: TrackType[]) => {
+        //     for (let index = 0; index < config.maxRounds / playlists.length; index++) {
+        //         const tracksArray: TrackType[] = [];
+        //         for (let answerIndex = 0; answerIndex < ANSWERS_COUNT; answerIndex++) {
+        //             this.getTrackOld(tracksArray, playlist, answerIndex);
+        //         }
+        //         const round: TrackType[] = shuffle(tracksArray);
+        //         this.answers.push(round);
+        //     }
+        // });
+
+        console.log('answersArray', answersArray[0]);
+        console.log('answersArray', answersArray[0].length);
     };
 
     get = (roundNumber: number): TrackType[] => {
